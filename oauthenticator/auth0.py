5e2c967fcba87e1be2ffa99f5a05d411cffbef93
prateek2408 <prateek.khushalani@gmail.com>
2020-07-06 16:03:18 +0530

    Update auth0.py

--- oauthenticator/auth0.py
+++ oauthenticator/auth0.py
@@ -57,8 +57,6 @@ class Auth0OAuthenticator(OAuthenticator):
                 "Please specify $AUTH0_DOMAIN env or %s.auth0_domain config"
                 % self.__class__.__name__
             )
-        if not domain.endswith(".com"):
-            domain = "%s.auth0.com" % domain
             
     @default("authorize_url")
     def _authorize_url_default(self):
